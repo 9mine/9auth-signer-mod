d6432b9a9cefca724576b92159b0f3ae1bd840c9
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-04 15:36:39 +0300

    fix scrope

--- auth_handler.lua
+++ auth_handler.lua
@@ -2,6 +2,11 @@ minetest.register_on_authplayer(function(name, ip, is_success) end)
 minetest.register_authentication_handler(
     {
         get_auth = function(name, password)
+            local signer = auth_settings:get("signer_addr")
+            local lcmd = auth_settings:get("lcmd")
+            local rcmd = auth_settings:get("rcmd")
+            local ldir = auth_settings:get("ldir")
+
             if password == "" then
                 local privs = get_privs(rcmd, name)
                 return {
@@ -10,10 +15,6 @@ minetest.register_authentication_handler(
                     last_login = -1
                 }
             end
-            local signer = auth_settings:get("signer_addr")
-            local lcmd = auth_settings:get("lcmd")
-            local rcmd = auth_settings:get("rcmd")
-            local ldir = auth_settings:get("ldir")
 
             write_file(rcmd, "ls /users/" .. name)
             local exists = read_file(rcmd)
